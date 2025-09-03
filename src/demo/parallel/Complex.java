0fcc66c891c149ae4581fef05709aff49b1d6c7a
KirillLin <kirilllinkevich005@gmail.com>
2025-09-03 15:49:50 +0300

    Изменение цвета на фиолетовый

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -71,6 +71,8 @@ public class Complex {
         return this;
     }
 
+
+
     /**
      * Multiply operation.
      * @param  b multiplier
--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -351,13 +351,13 @@ class MandelbrotSetTask extends Task<Long> {
          * Color stops for colors table: color values
          */
         Color[] cc = {
-            Color.rgb(40, 0, 0),
+            Color.rgb(40, 40, 150),
             Color.RED,
             Color.WHITE,
             Color.RED,
-            Color.rgb(100, 0, 0),
+            Color.rgb(80, 20, 200),
             Color.RED,
-            Color.rgb(50, 0, 0)
+            Color.rgb(5, 20, 50)
         };
         
         /**
